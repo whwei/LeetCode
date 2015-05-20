a37bc45b88bc924285f3f71445a5f9b77a1168f4
whwei <wanghongwei@51zhangdan.com.cn>
2015-05-20 11:27:02 +0800

    #32 longest valid parentheses note

--- longest-valid-parentheses/longest-valid-parentheses.js
+++ longest-valid-parentheses/longest-valid-parentheses.js
@@ -4,6 +4,8 @@
  */
 
 // time: O(n), space: O(n)
+// store invalid parentheses position in a stack,
+// then iterate the stack to get the maximum span.
 var longestValidParentheses = function(s) {
      var ss = s.split(''),
          stack = [],
@@ -35,6 +37,14 @@ var longestValidParentheses = function(s) {
 
 
 // time: O(n), space: O(1)
+// record parentheses depth to see if there is invalid `)`, such as "())".
+// if so, current sequence stops, reset `depth` and record the new substring start position `start`;
+// if depth === 0, check to see if length of the current substring is larger than previous one.
+//
+// for special case like "(()", `depth` will never reach 0 because of the invalid `(`, so we need
+// anther loop to iterate from the end of the string so that we can turn this special invalid `(`
+// to normal `)` case.
+// finally, compare the result of the two loop to get the final anwser.
 var longestValidParentheses = function(s) {
      var max = 0,
          depth = 0,
@@ -81,7 +91,16 @@ var longestValidParentheses = function(s) {
 
 
 
-// DP: time: O(n), space: O(n)
+/** DP: time: O(n), space: O(n)
+ *  subproblem: let D(i) be the length of the longest valid parentheses end with the ith element in given string.
+ *  recurrence: 1) if s[i] == '(', then D(i) = 0, for none valid parentheses ends with '('.
+ *              2) if s[i] == ')'
+ *                  2.1) s[i - 1] == '(', D(i) = D(i - 2) + 2 ("xxxxxx()").
+ *                  2.2) s[i - 1] == ')' &&  ss[i - D(i-1) - 1] == '(', D(i) = D(i - D(i - 1) - 2) + D(i - 1) + 2
+ *                      ("xxxxx(xxxxx)")
+ *  base case: D(0) = 0
+ *             D(1) = 0
+ */
 var longestValidParentheses = function(s) {
     if (s.length < 2) return 0;
 
