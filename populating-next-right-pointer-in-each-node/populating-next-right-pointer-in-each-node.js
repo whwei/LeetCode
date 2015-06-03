801607e407b928cd332e4b73f053aa9d07f4811d
whwei <wanghongwei@51zhangdan.com.cn>
2015-06-03 20:08:05 +0800

    #116 Populating Next Right Pointers in Each Node

---
+++ populating-next-right-pointer-in-each-node/populating-next-right-pointer-in-each-node.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for binary tree with next pointer.
+ * function TreeLinkNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = this.next = null;
+ * }
+ */
+
+/**
+ * @param {TreeLinkNode} root
+ * @return {void} Do not return anything, modify tree in-place instead.
+ */
+var connect = function(root) {
+    if(root == null)
+        return;
+
+    if(root.left != null){
+        root.left.next = root.right;
+        if(root.next != null)
+            root.right.next = root.next.left;
+    }
+
+    connect(root.left);
+    connect(root.right);
+};
