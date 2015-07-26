9b7a1c144e7a2a00948f455e784e5072d99da212
whwei <whw.0922@gmail.com>
2015-07-26 18:23:51 +0800

    Implement strStr()

---
+++ implement-strstr/implement-strstr.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
+
+
+
+// TODO: KMP
+
