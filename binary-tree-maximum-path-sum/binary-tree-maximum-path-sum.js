c08cfc000a7d6d539b7e169a8918fe82acceef2f
王宏伟 <wanghongwei@51zhangdan.com.cn>
2015-09-16 10:40:40 +0800

    Binary Tree Maximum Path Sum

---
+++ binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.js
@@ -0,0 +1,32 @@
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+    if (!root) return null
+
+    var result = { max: -Number.MAX_VALUE }
+
+    maxPath(root, result)
+
+    return result.max
+};
+
+function maxPath(root, result) {
+    if (!root) return 0
+
+    var l = maxPath(root.left, result)
+    var r = maxPath(root.right, result)
+
+    if (l + r + root.val > result.max) result.max = l + r + root.val
+
+    return Math.max(0, Math.max(l, r) + root.val)
+}
