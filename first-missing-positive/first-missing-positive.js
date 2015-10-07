37554e7b0d2aeb6f07426e4b07cb469e69ae5a03
whwei <whw.0922@gmail.com>
2015-10-07 18:53:55 +0800

    First Missing Positive

--- first-missing-positive/first-missing-positive.js
+++ first-missing-positive/first-missing-positive.js
@@ -17,3 +17,31 @@ var firstMissingPositive = function(nums) {
 
     return counter
 };
+
+
+
+var firstMissingPositive = function(nums) {
+    var i = 0,
+        n
+    for (; i < nums.length;) {
+        n = nums[i]
+        if (n <= 0 || n > nums.length || n === i + 1) {
+            i++
+        } else if (nums[n - 1] !== n) {
+            swap(nums, i, n - 1)
+        } else {
+            i++
+        }
+    }
+
+    var j = 0
+    while (j < nums.length && j + 1 === nums[j]) j++
+
+    return j + 1
+};
+
+function swap(arr, a, b) {
+    var tmp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = tmp
+}
