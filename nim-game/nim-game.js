7b7c70b97732e143033d231cf65786208d87a06f
王宏伟 <wanghongwei@51zhangdan.com.cn>
2015-10-13 10:31:45 +0800

    Nim Game

---
+++ nim-game/nim-game.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ **/
+var canWinNim = function(n) {
+    //var map = {
+        //0: false,
+        //1: true,
+        //2: true,
+        //3: true
+    //}
+    return n % 4 !== 0
+};
